6800700f6c46545ccd050f23ad0535ee32670957
Stanley Wintergreen <kepocnhh@protonmail.org>
2025-03-11 16:13:46 +0300

    rotations test

--- lib/src/main/kotlin/sp/kx/calculations/geometry/VertexRotations.kt
+++ lib/src/main/kotlin/sp/kx/calculations/geometry/VertexRotations.kt
@@ -1,6 +1,7 @@
 package sp.kx.calculations.geometry
 
 // https://en.wikipedia.org/wiki/Rotation_matrix
+
 fun rotate(
     x: Double,
     y: Double,
@@ -23,5 +24,36 @@ fun rotate(
         x = _x,
         y = _y * c - _z * s,
         z = _y * s + _z * c,
+    )
+}
+
+fun rotate(
+    x: Double,
+    y: Double,
+    z: Double,
+    aX: Double,
+    aY: Double,
+    aZ: Double,
+    rX: Double,
+    rY: Double,
+    rZ: Double,
+): Vertex {
+    val x0 = x - rX
+    val y0 = y - rY
+    val z0 = z - rZ
+    var c = kotlin.math.cos(aZ)
+    var s = kotlin.math.sin(aZ)
+    val _y = x0 * s + y0 * c
+    var _x = x0 * c - y0 * s
+    c = kotlin.math.cos(aY)
+    s = kotlin.math.sin(aY)
+    val _z = _x * s + z0 * c
+    _x = _x * c - z0 * s
+    c = kotlin.math.cos(aX)
+    s = kotlin.math.sin(aX)
+    return MutableVertex(
+        x = _x + rX,
+        y = _y * c - _z * s + rY,
+        z = _y * s + _z * c + rZ,
     )
 }
--- lib/src/test/kotlin/sp/kx/calculations/geometry/VertexRotationsTest.kt
+++ lib/src/test/kotlin/sp/kx/calculations/geometry/VertexRotationsTest.kt
@@ -113,4 +113,126 @@ internal class VertexRotationsTest {
             }
         }
     }
+
+    @Test
+    fun rotateAboutTest() {
+        val delta = 0.00000001
+        val exponent = 8
+        val pi12 = kotlin.math.PI / 2
+        val pi22 = kotlin.math.PI
+        val pi32 = kotlin.math.PI / 2 * 3
+        listOf(
+            MutableVertex(0.0, 0.0, 0.0) to listOf(
+                Triple(
+                    MutableRotation(0.0, 0.0, 0.0),
+                    MutableVertex(0.0, 0.0, 0.0),
+                    MutableVertex(0.0, 0.0, 0.0),
+                ),
+            ),
+            MutableVertex(1.0, 0.0, 0.0) to listOf(
+                Triple(
+                    MutableRotation(0.0, 0.0, 0.0),
+                    MutableVertex(0.0, 0.0, 0.0),
+                    MutableVertex(1.0, 0.0, 0.0),
+                ),
+            ),
+            MutableVertex(2.0, 0.0, 0.0) to listOf(
+                Triple(
+                    MutableRotation(0.0, 0.0, pi12),
+                    MutableVertex(1.0, 0.0, 0.0),
+                    MutableVertex(1.0, 1.0, 0.0),
+                ),
+                Triple(
+                    MutableRotation(0.0, 0.0, pi22),
+                    MutableVertex(1.0, 0.0, 0.0),
+                    MutableVertex(0.0, 0.0, 0.0),
+                ),
+                Triple(
+                    MutableRotation(0.0, 0.0, pi32),
+                    MutableVertex(1.0, 0.0, 0.0),
+                    MutableVertex(1.0, -1.0, 0.0),
+                ),
+            ),
+            MutableVertex(5.0, 0.0, 0.0) to listOf(
+                Triple(
+                    MutableRotation(0.0, 0.0, pi12),
+                    MutableVertex(3.0, 0.0, 0.0),
+                    MutableVertex(3.0, 2.0, 0.0),
+                ),
+                Triple(
+                    MutableRotation(0.0, 0.0, pi22),
+                    MutableVertex(3.0, 0.0, 0.0),
+                    MutableVertex(1.0, 0.0, 0.0),
+                ),
+                Triple(
+                    MutableRotation(0.0, 0.0, pi32),
+                    MutableVertex(3.0, 0.0, 0.0),
+                    MutableVertex(3.0, -2.0, 0.0),
+                ),
+                //
+                Triple(
+                    MutableRotation(0.0, pi12, 0.0),
+                    MutableVertex(3.0, 0.0, 0.0),
+                    MutableVertex(3.0, 0.0, 2.0),
+                ),
+                Triple(
+                    MutableRotation(0.0, pi22, 0.0),
+                    MutableVertex(3.0, 0.0, 0.0),
+                    MutableVertex(1.0, 0.0, 0.0),
+                ),
+                Triple(
+                    MutableRotation(0.0, pi32, 0.0),
+                    MutableVertex(3.0, 0.0, 0.0),
+                    MutableVertex(3.0, 0.0, -2.0),
+                ),
+            ),
+            MutableVertex(0.0, 5.0, 0.0) to listOf(
+                Triple(
+                    MutableRotation(pi12, 0.0, 0.0),
+                    MutableVertex(0.0, 3.0, 0.0),
+                    MutableVertex(0.0, 3.0, 2.0),
+                ),
+                Triple(
+                    MutableRotation(pi22, 0.0, 0.0),
+                    MutableVertex(0.0, 3.0, 0.0),
+                    MutableVertex(0.0, 1.0, 0.0),
+                ),
+                Triple(
+                    MutableRotation(pi32, 0.0, 0.0),
+                    MutableVertex(0.0, 3.0, 0.0),
+                    MutableVertex(0.0, 3.0, -2.0),
+                ),
+            ),
+        ).forEachIndexed { i, (vertex, issues) ->
+            issues.forEachIndexed { j, issue ->
+                val (rotation, about, expected) = issue
+                val actual = rotate(
+                    x = vertex.x,
+                    y = vertex.y,
+                    z = vertex.z,
+                    aX = rotation.aX,
+                    aY = rotation.aY,
+                    aZ = rotation.aZ,
+                    rX = about.x,
+                    rY = about.y,
+                    rZ = about.z,
+                )
+                val message = """
+                    $i/$j
+                    v: $vertex
+                    e: $expected
+                    a: $actual
+                    r: $rotation
+                    about: $about
+                """.trimIndent()
+                assertEquals(
+                    expected = expected,
+                    actual = actual,
+                    delta = delta,
+                    exponent = exponent,
+                    message = message,
+                )
+            }
+        }
+    }
 }
