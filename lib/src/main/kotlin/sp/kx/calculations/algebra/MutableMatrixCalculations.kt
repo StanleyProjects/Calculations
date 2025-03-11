b74210607367237157b7468fc5f765e1da05b7e2
Stanley Wintergreen <kepocnhh@protonmail.org>
2025-03-11 21:30:34 +0300

    matrix rotations

--- lib/src/main/kotlin/sp/kx/calculations/algebra/MutableMatrixCalculations.kt
+++ lib/src/main/kotlin/sp/kx/calculations/algebra/MutableMatrixCalculations.kt
@@ -13,3 +13,147 @@ fun MutableMatrix.translate(dX: Double, dY: Double, dZ: Double) {
     m23 += m20 * dX + m21 * dY + m22 * dZ
     m33 += m30 * dX + m31 * dY + m32 * dZ
 }
+
+fun MutableMatrix.rx(radians: Double) {
+    val c = kotlin.math.cos(radians)
+    val s = kotlin.math.sin(radians)
+    //
+    val n00 = 1.0; val n01 = 0.0; val n02 = 0.0; val n03 = 0.0;
+    val n10 = 0.0; val n11 = c; val n12 = -s; val n13 = 0.0;
+    val n20 = 0.0; val n21 = s; val n22 = c; val n23 = 0.0;
+    val n30 = 0.0; val n31 = 0.0; val n32 = 0.0; val n33 = 1.0;
+    //
+    val m00 = this.m00 * n00 + this.m01 * n10 + this.m02 * n20 + this.m03 * n30
+    val m01 = this.m00 * n01 + this.m01 * n11 + this.m02 * n21 + this.m03 * n31
+    val m02 = this.m00 * n02 + this.m01 * n12 + this.m02 * n22 + this.m03 * n32
+    val m03 = this.m00 * n03 + this.m01 * n13 + this.m02 * n23 + this.m03 * n33
+    //
+    val m10 = this.m10 * n00 + this.m11 * n10 + this.m12 * n20 + this.m13 * n30
+    val m11 = this.m10 * n01 + this.m11 * n11 + this.m12 * n21 + this.m13 * n31
+    val m12 = this.m10 * n02 + this.m11 * n12 + this.m12 * n22 + this.m13 * n32
+    val m13 = this.m10 * n03 + this.m11 * n13 + this.m12 * n23 + this.m13 * n33
+    //
+    val m20 = this.m20 * n00 + this.m21 * n10 + this.m22 * n20 + this.m23 * n30
+    val m21 = this.m20 * n01 + this.m21 * n11 + this.m22 * n21 + this.m23 * n31
+    val m22 = this.m20 * n02 + this.m21 * n12 + this.m22 * n22 + this.m23 * n32
+    val m23 = this.m20 * n03 + this.m21 * n13 + this.m22 * n23 + this.m23 * n33
+    //
+    val m30 = this.m30 * n00 + this.m31 * n10 + this.m32 * n20 + this.m33 * n30
+    val m31 = this.m30 * n01 + this.m31 * n11 + this.m32 * n21 + this.m33 * n31
+    val m32 = this.m30 * n02 + this.m31 * n12 + this.m32 * n22 + this.m33 * n32
+    val m33 = this.m30 * n03 + this.m31 * n13 + this.m32 * n23 + this.m33 * n33
+    //
+    this.m00 = m00; this.m01 = m01; this.m02 = m02; this.m03 = m03
+    this.m10 = m10; this.m11 = m11; this.m12 = m12; this.m13 = m13
+    this.m20 = m20; this.m21 = m21; this.m22 = m22; this.m23 = m23
+    this.m30 = m30; this.m31 = m31; this.m32 = m32; this.m33 = m33
+}
+
+fun MutableMatrix.ry(radians: Double) {
+    val c = kotlin.math.cos(radians)
+    val s = kotlin.math.sin(radians)
+    //
+    val n00 = c; val n01 = 0.0; val n02 = -s; val n03 = 0.0;
+    val n10 = 0.0; val n11 = 1.0; val n12 = 0.0; val n13 = 0.0;
+    val n20 = s; val n21 = 0.0; val n22 = c; val n23 = 0.0;
+    val n30 = 0.0; val n31 = 0.0; val n32 = 0.0; val n33 = 1.0;
+    //
+    val m00 = this.m00 * n00 + this.m01 * n10 + this.m02 * n20 + this.m03 * n30
+    val m01 = this.m00 * n01 + this.m01 * n11 + this.m02 * n21 + this.m03 * n31
+    val m02 = this.m00 * n02 + this.m01 * n12 + this.m02 * n22 + this.m03 * n32
+    val m03 = this.m00 * n03 + this.m01 * n13 + this.m02 * n23 + this.m03 * n33
+    //
+    val m10 = this.m10 * n00 + this.m11 * n10 + this.m12 * n20 + this.m13 * n30
+    val m11 = this.m10 * n01 + this.m11 * n11 + this.m12 * n21 + this.m13 * n31
+    val m12 = this.m10 * n02 + this.m11 * n12 + this.m12 * n22 + this.m13 * n32
+    val m13 = this.m10 * n03 + this.m11 * n13 + this.m12 * n23 + this.m13 * n33
+    //
+    val m20 = this.m20 * n00 + this.m21 * n10 + this.m22 * n20 + this.m23 * n30
+    val m21 = this.m20 * n01 + this.m21 * n11 + this.m22 * n21 + this.m23 * n31
+    val m22 = this.m20 * n02 + this.m21 * n12 + this.m22 * n22 + this.m23 * n32
+    val m23 = this.m20 * n03 + this.m21 * n13 + this.m22 * n23 + this.m23 * n33
+    //
+    val m30 = this.m30 * n00 + this.m31 * n10 + this.m32 * n20 + this.m33 * n30
+    val m31 = this.m30 * n01 + this.m31 * n11 + this.m32 * n21 + this.m33 * n31
+    val m32 = this.m30 * n02 + this.m31 * n12 + this.m32 * n22 + this.m33 * n32
+    val m33 = this.m30 * n03 + this.m31 * n13 + this.m32 * n23 + this.m33 * n33
+    //
+    this.m00 = m00; this.m01 = m01; this.m02 = m02; this.m03 = m03
+    this.m10 = m10; this.m11 = m11; this.m12 = m12; this.m13 = m13
+    this.m20 = m20; this.m21 = m21; this.m22 = m22; this.m23 = m23
+    this.m30 = m30; this.m31 = m31; this.m32 = m32; this.m33 = m33
+}
+
+fun MutableMatrix.rz(radians: Double) {
+    val c = kotlin.math.cos(radians)
+    val s = kotlin.math.sin(radians)
+    //
+    val n00 = c; val n01 = -s; val n02 = 0.0; val n03 = 0.0;
+    val n10 = s; val n11 = c; val n12 = 0.0; val n13 = 0.0;
+    val n20 = 0.0; val n21 = 0.0; val n22 = 1.0; val n23 = 0.0;
+    val n30 = 0.0; val n31 = 0.0; val n32 = 0.0; val n33 = 1.0;
+    //
+    val m00 = this.m00 * n00 + this.m01 * n10 + this.m02 * n20 + this.m03 * n30
+    val m01 = this.m00 * n01 + this.m01 * n11 + this.m02 * n21 + this.m03 * n31
+    val m02 = this.m00 * n02 + this.m01 * n12 + this.m02 * n22 + this.m03 * n32
+    val m03 = this.m00 * n03 + this.m01 * n13 + this.m02 * n23 + this.m03 * n33
+    //
+    val m10 = this.m10 * n00 + this.m11 * n10 + this.m12 * n20 + this.m13 * n30
+    val m11 = this.m10 * n01 + this.m11 * n11 + this.m12 * n21 + this.m13 * n31
+    val m12 = this.m10 * n02 + this.m11 * n12 + this.m12 * n22 + this.m13 * n32
+    val m13 = this.m10 * n03 + this.m11 * n13 + this.m12 * n23 + this.m13 * n33
+    //
+    val m20 = this.m20 * n00 + this.m21 * n10 + this.m22 * n20 + this.m23 * n30
+    val m21 = this.m20 * n01 + this.m21 * n11 + this.m22 * n21 + this.m23 * n31
+    val m22 = this.m20 * n02 + this.m21 * n12 + this.m22 * n22 + this.m23 * n32
+    val m23 = this.m20 * n03 + this.m21 * n13 + this.m22 * n23 + this.m23 * n33
+    //
+    val m30 = this.m30 * n00 + this.m31 * n10 + this.m32 * n20 + this.m33 * n30
+    val m31 = this.m30 * n01 + this.m31 * n11 + this.m32 * n21 + this.m33 * n31
+    val m32 = this.m30 * n02 + this.m31 * n12 + this.m32 * n22 + this.m33 * n32
+    val m33 = this.m30 * n03 + this.m31 * n13 + this.m32 * n23 + this.m33 * n33
+    //
+    this.m00 = m00; this.m01 = m01; this.m02 = m02; this.m03 = m03
+    this.m10 = m10; this.m11 = m11; this.m12 = m12; this.m13 = m13
+    this.m20 = m20; this.m21 = m21; this.m22 = m22; this.m23 = m23
+    this.m30 = m30; this.m31 = m31; this.m32 = m32; this.m33 = m33
+}
+
+fun MutableMatrix.rotate(aX: Double, aY: Double, aZ: Double) {
+    val cX = kotlin.math.cos(aX)
+    val cY = kotlin.math.cos(aY)
+    val cZ = kotlin.math.cos(aZ)
+    val sX = kotlin.math.sin(aX)
+    val sY = kotlin.math.sin(aY)
+    val sZ = kotlin.math.sin(aZ)
+    //
+    val n00 = cZ * cY; val n01 = cZ * sY * sX - sZ * cX; val n02 = cZ * sY * cX + sZ * sX; val n03 = 0.0;
+    val n10 = sZ * cY; val n11 = sZ * sY * sX + cZ * cX; val n12 = sZ * sY * cX - cZ * sX; val n13 = 0.0;
+    val n20 = -sY;     val n21 = cY * sX;                val n22 = cY * cX;                val n23 = 0.0;
+    val n30 = 0.0;     val n31 = 0.0;                    val n32 = 0.0;                    val n33 = 1.0;
+    //
+    val m00 = this.m00 * n00 + this.m01 * n10 + this.m02 * n20 + this.m03 * n30
+    val m01 = this.m00 * n01 + this.m01 * n11 + this.m02 * n21 + this.m03 * n31
+    val m02 = this.m00 * n02 + this.m01 * n12 + this.m02 * n22 + this.m03 * n32
+    val m03 = this.m00 * n03 + this.m01 * n13 + this.m02 * n23 + this.m03 * n33
+    //
+    val m10 = this.m10 * n00 + this.m11 * n10 + this.m12 * n20 + this.m13 * n30
+    val m11 = this.m10 * n01 + this.m11 * n11 + this.m12 * n21 + this.m13 * n31
+    val m12 = this.m10 * n02 + this.m11 * n12 + this.m12 * n22 + this.m13 * n32
+    val m13 = this.m10 * n03 + this.m11 * n13 + this.m12 * n23 + this.m13 * n33
+    //
+    val m20 = this.m20 * n00 + this.m21 * n10 + this.m22 * n20 + this.m23 * n30
+    val m21 = this.m20 * n01 + this.m21 * n11 + this.m22 * n21 + this.m23 * n31
+    val m22 = this.m20 * n02 + this.m21 * n12 + this.m22 * n22 + this.m23 * n32
+    val m23 = this.m20 * n03 + this.m21 * n13 + this.m22 * n23 + this.m23 * n33
+    //
+    val m30 = this.m30 * n00 + this.m31 * n10 + this.m32 * n20 + this.m33 * n30
+    val m31 = this.m30 * n01 + this.m31 * n11 + this.m32 * n21 + this.m33 * n31
+    val m32 = this.m30 * n02 + this.m31 * n12 + this.m32 * n22 + this.m33 * n32
+    val m33 = this.m30 * n03 + this.m31 * n13 + this.m32 * n23 + this.m33 * n33
+    //
+    this.m00 = m00; this.m01 = m01; this.m02 = m02; this.m03 = m03
+    this.m10 = m10; this.m11 = m11; this.m12 = m12; this.m13 = m13
+    this.m20 = m20; this.m21 = m21; this.m22 = m22; this.m23 = m23
+    this.m30 = m30; this.m31 = m31; this.m32 = m32; this.m33 = m33
+}
--- lib/src/test/kotlin/sp/kx/calculations/algebra/MutableMatrixCalculationsTest.kt
+++ lib/src/test/kotlin/sp/kx/calculations/algebra/MutableMatrixCalculationsTest.kt
@@ -2,6 +2,7 @@ package sp.kx.calculations.algebra
 
 import org.junit.jupiter.api.Test
 import sp.kx.calculations.Assertions.assertEquals
+import sp.kx.calculations.geometry.MutableRotation
 import sp.kx.calculations.geometry.MutableVertex
 import sp.kx.calculations.operators.times
 
@@ -24,5 +25,218 @@ internal class MutableMatrixCalculationsTest {
             a: $actual
         """.trimIndent()
         assertEquals(expected = expected, actual = actual, delta = delta, exponent = exponent, message = message)
+    }
+
+    @Test
+    fun rotateTest() {
+        val delta = 0.00000001
+        val exponent = 8
+        val pi12 = kotlin.math.PI / 2
+        val pi22 = kotlin.math.PI
+        val pi32 = kotlin.math.PI / 2 * 3
+        val matrix = MutableMatrix()
+        listOf(
+            MutableVertex(1.0, 0.0, 0.0) to listOf(
+                // 00
+                Pair(
+                    MutableRotation(0.0, 0.0, 0.0),
+                    MutableVertex(1.0, 0.0, 0.0),
+                ),
+                // 01
+                Pair(
+                    MutableRotation(0.0, 0.0, pi12),
+                    MutableVertex(0.0, 1.0, 0.0),
+                ),
+                Pair(
+                    MutableRotation(0.0, 0.0, pi22),
+                    MutableVertex(-1.0, 0.0, 0.0),
+                ),
+                Pair(
+                    MutableRotation(0.0, 0.0, pi32),
+                    MutableVertex(0.0, -1.0, 0.0),
+                ),
+                // 04
+                Pair(
+                    MutableRotation(0.0, pi12, 0.0),
+                    MutableVertex(0.0, 0.0, 1.0),
+                ),
+                Pair(
+                    MutableRotation(0.0, pi22, 0.0),
+                    MutableVertex(-1.0, 0.0, 0.0),
+                ),
+                Pair(
+                    MutableRotation(0.0, pi32, 0.0),
+                    MutableVertex(0.0, 0.0, -1.0),
+                ),
+                // 07
+                Pair(
+                    MutableRotation(pi12, 0.0, pi12),
+                    MutableVertex(0.0, 0.0, 1.0),
+                ),
+                Pair(
+                    MutableRotation(pi12, pi12, 0.0),
+                    MutableVertex(0.0, -1.0, 0.0),
+                ),
+                Pair(
+                    MutableRotation(-pi12, pi12, 0.0),
+                    MutableVertex(0.0, 1.0, 0.0),
+                ),
+            ),
+        ).forEachIndexed { i, (vertex, issues) ->
+            issues.forEachIndexed { j, (rotation, expected) ->
+                matrix.identity()
+                matrix.rotate(aX = rotation.aX, aY = rotation.aY, aZ = rotation.aZ)
+//                matrix.rotate(aX = rotation.aX, aY = 0.0, aZ = 0.0)
+//                matrix.rotate(aX = 0.0, aY = rotation.aY, aZ = 0.0)
+//                matrix.rotate(aX = 0.0, aY = 0.0, aZ = rotation.aZ)
+                val actual = vertex * matrix
+                val message = """
+                    $i/$j
+                    v: $vertex
+                    e: $expected
+                    a: $actual
+                    r: $rotation
+                """.trimIndent()
+                assertEquals(expected = expected, actual = actual, delta = delta, exponent = exponent, message = message)
+            }
+        }
+    }
+
+    @Test
+    fun rxTest() {
+        val delta = 0.00000001
+        val exponent = 8
+        val pi12 = kotlin.math.PI / 2
+        val pi22 = kotlin.math.PI
+        val pi32 = kotlin.math.PI / 2 * 3
+        val matrix = MutableMatrix()
+        listOf(
+            MutableVertex(0.0, 1.0, 0.0) to listOf(
+                // 00
+                Pair(
+                    0.0,
+                    MutableVertex(0.0, 1.0, 0.0),
+                ),
+                // 01
+                Pair(
+                    pi12,
+                    MutableVertex(0.0, 0.0, 1.0),
+                ),
+                Pair(
+                    pi22,
+                    MutableVertex(0.0, -1.0, 0.0),
+                ),
+                Pair(
+                    pi32,
+                    MutableVertex(0.0, 0.0, -1.0),
+                ),
+            ),
+        ).forEachIndexed { i, (vertex, issues) ->
+            issues.forEachIndexed { j, (radians, expected) ->
+                matrix.identity()
+                matrix.rx(radians = radians)
+                val actual = vertex * matrix
+                val message = """
+                    $i/$j
+                    v: $vertex
+                    e: $expected
+                    a: $actual
+                    radians: $radians
+                """.trimIndent()
+                assertEquals(expected = expected, actual = actual, delta = delta, exponent = exponent, message = message)
+            }
+        }
+    }
+
+    @Test
+    fun ryTest() {
+        val delta = 0.00000001
+        val exponent = 8
+        val pi12 = kotlin.math.PI / 2
+        val pi22 = kotlin.math.PI
+        val pi32 = kotlin.math.PI / 2 * 3
+        val matrix = MutableMatrix()
+        listOf(
+            MutableVertex(1.0, 0.0, 0.0) to listOf(
+                // 00
+                Pair(
+                    0.0,
+                    MutableVertex(1.0, 0.0, 0.0),
+                ),
+                // 01
+                Pair(
+                    pi12,
+                    MutableVertex(0.0, 0.0, 1.0),
+                ),
+                Pair(
+                    pi22,
+                    MutableVertex(-1.0, 0.0, 0.0),
+                ),
+                Pair(
+                    pi32,
+                    MutableVertex(0.0, 0.0, -1.0),
+                ),
+            ),
+        ).forEachIndexed { i, (vertex, issues) ->
+            issues.forEachIndexed { j, (radians, expected) ->
+                matrix.identity()
+                matrix.ry(radians = radians)
+                val actual = vertex * matrix
+                val message = """
+                    $i/$j
+                    v: $vertex
+                    e: $expected
+                    a: $actual
+                    radians: $radians
+                """.trimIndent()
+                assertEquals(expected = expected, actual = actual, delta = delta, exponent = exponent, message = message)
+            }
+        }
+    }
+
+    @Test
+    fun rzTest() {
+        val delta = 0.00000001
+        val exponent = 8
+        val pi12 = kotlin.math.PI / 2
+        val pi22 = kotlin.math.PI
+        val pi32 = kotlin.math.PI / 2 * 3
+        val matrix = MutableMatrix()
+        listOf(
+            MutableVertex(1.0, 0.0, 0.0) to listOf(
+                // 00
+                Pair(
+                    0.0,
+                    MutableVertex(1.0, 0.0, 0.0),
+                ),
+                // 01
+                Pair(
+                    pi12,
+                    MutableVertex(0.0, 1.0, 0.0),
+                ),
+                Pair(
+                    pi22,
+                    MutableVertex(-1.0, 0.0, 0.0),
+                ),
+                Pair(
+                    pi32,
+                    MutableVertex(0.0, -1.0, 0.0),
+                ),
+            ),
+        ).forEachIndexed { i, (vertex, issues) ->
+            issues.forEachIndexed { j, (radians, expected) ->
+                matrix.identity()
+                matrix.rz(radians = radians)
+                val actual = vertex * matrix
+                val message = """
+                    $i/$j
+                    v: $vertex
+                    e: $expected
+                    a: $actual
+                    radians: $radians
+                """.trimIndent()
+                assertEquals(expected = expected, actual = actual, delta = delta, exponent = exponent, message = message)
+            }
+        }
     }
 }
