a2da57647f44c7692673153c40b840a53b711d63
Stanley Wintergreen <kepocnhh@protonmail.org>
2025-03-11 13:29:00 +0300

    comparisons

--- lib/src/main/kotlin/sp/kx/calculations/Size.kt
+++ lib/src/main/kotlin/sp/kx/calculations/Size.kt
@@ -1,6 +1,17 @@
 package sp.kx.calculations
 
+import sp.kx.calculations.geometry.MutableOffset
+import sp.kx.calculations.geometry.Offset
+
 interface Size {
     val width: Double
     val height: Double
+}
+
+fun Size.center(dZ: Double): Offset {
+    return MutableOffset(
+        dX = width / 2,
+        dY = height / 2,
+        dZ = dZ,
+    )
 }
--- lib/src/main/kotlin/sp/kx/calculations/operators/SizeOperators.kt
+++ lib/src/main/kotlin/sp/kx/calculations/operators/SizeOperators.kt
@@ -2,8 +2,6 @@ package sp.kx.calculations.operators
 
 import sp.kx.calculations.MutableSize
 import sp.kx.calculations.Size
-import sp.kx.calculations.geometry.MutableOffset
-import sp.kx.calculations.geometry.Offset
 
 operator fun Size.times(scale: Double): Size {
     return MutableSize(
@@ -16,13 +14,5 @@ operator fun Size.div(scale: Double): Size {
     return MutableSize(
         width = width / scale,
         height = height / scale,
-    )
-}
-
-fun Size.center(dZ: Double): Offset {
-    return MutableOffset(
-        dX = width / 2,
-        dY = height / 2,
-        dZ = dZ,
     )
 }
