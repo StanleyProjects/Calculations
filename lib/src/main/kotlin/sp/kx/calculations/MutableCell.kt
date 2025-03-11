a955329454813e19aa1313d7e4fda526434a5667
Stanley Wintergreen <kepocnhh@protonmail.org>
2025-03-11 16:39:56 +0300

    fix cell

--- lib/src/main/kotlin/sp/kx/calculations/MutableCell.kt
+++ lib/src/main/kotlin/sp/kx/calculations/MutableCell.kt
@@ -3,8 +3,8 @@ package sp.kx.calculations
 import java.util.Objects
 
 class MutableCell(
-    override val x: Int,
-    override val y: Int,
+    override var x: Int,
+    override var y: Int,
 ) : Cell {
     override fun equals(other: Any?): Boolean {
         return when (other) {
