9ce9832ab835f2c52bb4f4b12ccbed04b9488180
Stanley Wintergreen <kepocnhh@protonmail.org>
2025-03-14 16:28:29 +0300

    0.3.1

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -14,7 +14,7 @@ import sp.gx.core.filled
 import sp.gx.core.task
 import kotlin.time.Duration.Companion.seconds
 
-version = "0.3.0"
+version = "0.3.1"
 
 val maven = Maven.Artifact(
     group = "com.github.kepocnhh",
