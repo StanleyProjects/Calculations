37d3a2ec73e1e33eb661b6e1c9571c1ece63bdf1
Stanley Wintergreen <kepocnhh@protonmail.org>
2025-03-15 19:54:34 +0300

    matrix mut rotations: xyz

--- lib/src/main/kotlin/sp/kx/calculations/rotations/MutableMatrixRotations.kt
+++ lib/src/main/kotlin/sp/kx/calculations/rotations/MutableMatrixRotations.kt
@@ -97,10 +97,6 @@ fun MutableMatrix.rxyz(
     aY: Double,
     aZ: Double,
 ) {
-//    rz(radians = aZ)
-//    ry(radians = aY)
-//    rx(radians = aX)
-    //
     val cX = kotlin.math.cos(aX)
     val cY = kotlin.math.cos(aY)
     val cZ = kotlin.math.cos(aZ)
@@ -108,56 +104,25 @@ fun MutableMatrix.rxyz(
     val sY = kotlin.math.sin(aY)
     val sZ = kotlin.math.sin(aZ)
     //
-    val x00 = 1.0; val x01 = 0.0; val x02 = 0.0; val x03 = 0.0
-    val x10 = 0.0; val x11 = cX;  val x12 = -sX; val x13 = 0.0
-    val x20 = 0.0; val x21 = sX;  val x22 = cX;  val x23 = 0.0
-    val x30 = 0.0; val x31 = 0.0; val x32 = 0.0; val x33 = 1.0
+    val n00 = cZ * cY
+    val n01 = cZ * sY * sX - sZ * cX
+    val n02 = cZ * sY * cX + sZ * sX
+    val n10 = sZ * cY
+    val n11 = cZ * cX + sZ * sY * sX
+    val n12 = sZ * sY * cX - cZ * sX
+    val n21 = cY * sX
+    val n22 = cY * cX
     //
-    val y00 = cY;  val y01 = 0.0; val y02 = sY;  val y03 = 0.0
-    val y10 = 0.0; val y11 = 1.0; val y12 = 0.0; val y13 = 0.0
-    val y20 = -sY; val y21 = 0.0; val y22 = cY;  val y23 = 0.0
-    val y30 = 0.0; val y31 = 0.0; val y32 = 0.0; val y33 = 1.0
-    //
-    val z00 = cZ;  val z01 = -sZ; val z02 = 0.0; val z03 = 0.0
-    val z10 = sZ;  val z11 = cZ;  val z12 = 0.0; val z13 = 0.0
-    val z20 = 0.0; val z21 = 0.0; val z22 = 1.0; val z23 = 0.0
-    val z30 = 0.0; val z31 = 0.0; val z32 = 0.0; val z33 = 1.0
-    //
-    val a00 = z00 * y00; val a01 = z01; val a02 = z00 * y02; val a03 = 0.0
-    val a10 = z10 * y00; val a11 = z11; val a12 = z10 * y02; val a13 = 0.0
-    val a20 = y20;       val a21 = 0.0; val a22 = y22;       val a23 = 0.0
-    val a30 = 0.0;       val a31 = 0.0; val a32 = 0.0;       val a33 = 1.0
-    //
-    val n00 = a00 * x00 + a01 * x10 + a02 * x20 + a03 * x30
-    val n01 = a00 * x01 + a01 * x11 + a02 * x21 + a03 * x31
-    val n02 = a00 * x02 + a01 * x12 + a02 * x22 + a03 * x32
-    val n03 = a00 * x03 + a01 * x13 + a02 * x23 + a03 * x33
-    val n10 = a10 * x00 + a11 * x10 + a12 * x20 + a13 * x30
-    val n11 = a10 * x01 + a11 * x11 + a12 * x21 + a13 * x31
-    val n12 = a10 * x02 + a11 * x12 + a12 * x22 + a13 * x32
-    val n13 = a10 * x03 + a11 * x13 + a12 * x23 + a13 * x33
-    val n20 = a20 * x00 + a21 * x10 + a22 * x20 + a23 * x30
-    val n21 = a20 * x01 + a21 * x11 + a22 * x21 + a23 * x31
-    val n22 = a20 * x02 + a21 * x12 + a22 * x22 + a23 * x32
-    val n23 = a20 * x03 + a21 * x13 + a22 * x23 + a23 * x33
-    val n30 = a30 * x00 + a31 * x10 + a32 * x20 + a33 * x30
-    val n31 = a30 * x01 + a31 * x11 + a32 * x21 + a33 * x31
-    val n32 = a30 * x02 + a31 * x12 + a32 * x22 + a33 * x32
-    val n33 = a30 * x03 + a31 * x13 + a32 * x23 + a33 * x33
-    //
-    val m00 = this.m00 * n00 + this.m01 * n10 + this.m02 * n20
+    val m00 = this.m00 * n00 + this.m01 * n10 - this.m02 * sY
     val m01 = this.m00 * n01 + this.m01 * n11 + this.m02 * n21
     val m02 = this.m00 * n02 + this.m01 * n12 + this.m02 * n22
-    //
-    val m10 = this.m10 * n00 + this.m11 * n10 + this.m12 * n20
+    val m10 = this.m10 * n00 + this.m11 * n10 - this.m12 * sY
     val m11 = this.m10 * n01 + this.m11 * n11 + this.m12 * n21
     val m12 = this.m10 * n02 + this.m11 * n12 + this.m12 * n22
-    //
-    val m20 = this.m20 * n00 + this.m21 * n10 + this.m22 * n20
+    val m20 = this.m20 * n00 + this.m21 * n10 - this.m22 * sY
     val m21 = this.m20 * n01 + this.m21 * n11 + this.m22 * n21
     val m22 = this.m20 * n02 + this.m21 * n12 + this.m22 * n22
-    //
-    val m30 = this.m30 * n00 + this.m31 * n10 + this.m32 * n20
+    val m30 = this.m30 * n00 + this.m31 * n10 - this.m32 * sY
     val m31 = this.m30 * n01 + this.m31 * n11 + this.m32 * n21
     val m32 = this.m30 * n02 + this.m31 * n12 + this.m32 * n22
     //
